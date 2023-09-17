abd078ac0906276dc0cf217fc62552fca4682ff9
SWAROOP Shivanna <swarooprooney@gmail.com>
2023-09-17 12:00:04 +0100

    Adding in-memory cosmos repository for functional tests

--- cosmos-container-int-test/PersonControllerTests.cs
+++ cosmos-container-int-test/PersonControllerTests.cs
@@ -6,7 +6,7 @@ namespace cosmos_container_int_test;
 public class PersonControllerTests : IntegrationTestBase
 {
     [TestMethod]
-    public async Task GetPersons_WhenPersonDoesntExist_ReturnsNotFound()
+    public async Task GetPerson_WhenPersonDoesntExist_ReturnsNotFound()
     {
         var id = Guid.NewGuid();
         var response = await _httpClient.GetAsync($"Person/{id}");
